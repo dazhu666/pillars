366f3a1ae98c8e32d152220234d95ef66397c142
dazhu666 <827489423@qq.com>
2017-01-12 15:54:54 +0800

    创建idea maven项目

---
+++ src/main/java/com/pillars/model/user.java
@@ -0,0 +1,65 @@
+package com.pillars.model;
+
+public class user {
+    private Long id;
+
+    private String name;
+
+    private String mobile;
+
+    private String login;
+
+    private String password;
+
+    public user(Long id, String name, String mobile, String login, String password) {
+        this.id = id;
+        this.name = name;
+        this.mobile = mobile;
+        this.login = login;
+        this.password = password;
+    }
+
+    public user() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile == null ? null : mobile.trim();
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login == null ? null : login.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+}
