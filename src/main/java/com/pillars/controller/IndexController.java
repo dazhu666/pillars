726724132797bee6318a5b31de6e3535a57b1a55
dazhu666 <827489423@qq.com>
2017-01-12 16:37:43 +0800

    新增菜单接口

--- src/main/java/com/pillars/controller/IndexController.java
+++ src/main/java/com/pillars/controller/IndexController.java
@@ -1,19 +1,51 @@
 package com.pillars.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
-/**
- * Created by Administrator on 2017/1/11.
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+
 @RequestMapping("/index")
 @Controller
 public class IndexController {
 
-    @RequestMapping("/home")
-    public String index(ModelMap params){
-
-        return "index/index";
-    }
+	@RequestMapping("/index")
+	public ModelAndView index(){
+		ModelAndView modelAndView=new ModelAndView();
+		modelAndView.setViewName("index");
+		return modelAndView;
+	}
+	@RequestMapping("/login")
+	public ModelAndView login(){
+		ModelAndView modelAndView=new ModelAndView();
+		modelAndView.setViewName("login");
+		return modelAndView;
+	}
+	@RequestMapping("/home")
+	public ModelAndView home(){
+		ModelAndView modelAndView=new ModelAndView();
+		modelAndView.setViewName("home");
+		return modelAndView;
+	}
+	@RequestMapping("/menu")
+	@ResponseBody
+	public Map<String,Object> menu(){
+		//MenuService service=new MenuService();
+		Map<String,Object> rs=new HashMap<String, Object>();
+		//rs.put("data", service.getMenu());
+		
+		return rs;
+	}
+	
+	@RequestMapping("/menus")
+	public ModelAndView menus(){
+		ModelAndView modelAndView=new ModelAndView();
+		modelAndView.setViewName("menu");
+		return modelAndView;
+	} 
 }
---
+++ src/main/java/com/pillars/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.pillars.controller;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.pillars.dao.UserMapper;
+import com.pillars.model.User;
+import com.pillars.utils.MyBatisUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+	
+	@RequestMapping("/register")
+	public @ResponseBody Map register(String login,String password,String mobile){
+		
+	/*UserDao dao=new UserDao();
+	dao.saveUser(login, mobile, login, password);*/
+	SqlSessionFactory sessionFactory= MyBatisUtil.getSqlSessionFactory();
+		SqlSession sqlSession=sessionFactory.openSession();
+		UserMapper userMapper=sqlSession.getMapper(UserMapper.class);
+		User user=new User();
+		user.setLogin(login);
+		user.setPassword(password);
+		user.setMobile(mobile);
+		userMapper.insert(user);
+		sqlSession.commit();
+		sqlSession.close();
+
+	
+	Map map=new HashMap();
+	map.put("message", "注册成功！");
+		return map;
+	}
+
+}
