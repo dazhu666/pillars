b8a064c50f809f1dad88fa4655816b088c0a5899
dazhu666 <827489423@qq.com>
2017-01-12 16:55:03 +0800

    新增菜单接口

---
+++ src/main/java/com/pillars/utils/MyBatisUtil.java
@@ -0,0 +1,30 @@
+package com.pillars.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * Created by Administrator on 2017/1/12.
+ */
+public class MyBatisUtil {
+    private final static SqlSessionFactory sqlSessionFactory;
+    static {
+        String resource = "mybatis-config.xml";
+        Reader reader = null;
+        try {
+            reader = Resources.getResourceAsReader(resource);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+
+        }
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+    }
+
+    public static SqlSessionFactory getSqlSessionFactory() {
+        return sqlSessionFactory;
+    }
+}
